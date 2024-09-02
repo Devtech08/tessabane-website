c8df8c9be2a37513dc71317472a099ae2a75d28e
Devtech08 <dadaprince011@gmail.com>
2024-09-02 13:37:18 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load',function(){
+document.body.classList.add('loaded');
+
+document.getElementById('transistion-overlay').
+style.transform='translateX(-100%)';
+});
+
+
+
